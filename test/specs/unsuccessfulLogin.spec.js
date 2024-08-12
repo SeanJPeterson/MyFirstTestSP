aea9935f5b755b899df93b8d5d4c0e1a64ec8c33
SeanJPeterson <sean.j.peterson@gmail.com>
2024-08-12 14:21:25 -0700

    first commit

--- test/specs/unsuccessfulLogin.spec.js
+++ test/specs/unsuccessfulLogin.spec.js
@@ -3,7 +3,7 @@ import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
 describe.skip('My unsuccessful Login application', () => { //describe is a test suite
-
+    //Username incorrect, password correct.
     it('should reject the invalid username', async () => { //it are individual tests
         await LoginPage.open()
 
@@ -11,7 +11,7 @@ describe.skip('My unsuccessful Login application', () => { //describe is a test 
         await expect(SecurePage.flashAlert).toBeExisting()
         await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining('Your username is invalid!'))
     })
-
+    //Username correct, password incorrect.
     it('should reject the invalid password', async () => {
         await LoginPage.open()
 
@@ -19,7 +19,7 @@ describe.skip('My unsuccessful Login application', () => { //describe is a test 
         await expect(SecurePage.flashAlert).toBeExisting()
         await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining('Your password is invalid!'))
     })
-
+    //Username and password incorrect.
     it('should reject the invalid username and password', async () => {
         await LoginPage.open()
 
